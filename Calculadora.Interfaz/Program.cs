78ee6df408c110cc28c50a900c9ecaede9282579
Dan Tcheche <dtcheche@gmail.com>
2021-03-16 22:55:35 -0300

    Setup inicial de la aplicacion.

---
+++ Calculadora.Interfaz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Interfaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valorSalir = "X";
+            string opcionIngresada = null;
+            while (opcionIngresada != valorSalir)
+            {
+                MostrarMenu();
+                opcionIngresada = PedirOpcionMenu();
+                if (opcionIngresada == 1)
+                {
+                    AgregarArticulo(codigoArticulo, descripcion, precio, articulosIngresados, maximoArticulos);
+                    articulosIngresados++;
+                }
+                else if (opcionIngresada == 2)
+                {
+                    ListarArticulos(codigoArticulo, descripcion, precio, articulosIngresados);
+                }
+                else if (opcionIngresada == valorSalir)
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
