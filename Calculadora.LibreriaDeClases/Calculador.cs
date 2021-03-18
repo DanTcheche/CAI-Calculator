e7a4e1b95ac56821871c7e4038ed6ba1468cfdb0
Dan Tcheche <dtcheche@gmail.com>
2021-03-17 23:29:45 -0300

    Agregar calculador para obtener resultado.

---
+++ Calculadora.LibreriaDeClases/Calculador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.LibreriaDeClases
+{
+    public class Calculador
+    {
+        public static float CalcularResultado(int primerNumero, string operador, int segundoNumero)
+        {
+            if(operador == "+") {
+                return primerNumero + segundoNumero;
+            } else if (operador == "-") {
+                return primerNumero - segundoNumero;
+            } else if (operador == "%") {
+                return primerNumero % segundoNumero;
+            } else {
+                return primerNumero * segundoNumero;
+            }
+        }
+    }
+}
