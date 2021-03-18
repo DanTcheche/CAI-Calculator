29bebd9088a2dbb6cd6ee5265fc7628ec4d9e5a6
Dan Tcheche <dtcheche@gmail.com>
2021-03-17 23:30:04 -0300

    Agregar proyectos.

--- Calculadora.Interfaz/Program.cs
+++ Calculadora.Interfaz/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculadora.LibreriaDeClases;
 
 namespace Calculadora.Interfaz
 {
@@ -14,7 +15,14 @@ namespace Calculadora.Interfaz
             string opcionIngresada = null;
             while (opcionIngresada != valorSalir)
             {
-
+                Console.WriteLine("Para salir ingrese X o cualquier otra cosa para continuar");
+                opcionIngresada = Interactor.PedirOpcionMenu().ToUpper();
+                Console.WriteLine(opcionIngresada);
+                int primerNumero = Interactor.PedirInt("Ingrese el primer digito.");
+                string operador = Interactor.PedirOperador("Ingrese un operador (-, +, % o *)"); 
+                int segundoNumero = Interactor.PedirInt("Ingrese el primer digito.");
+                float resultado = Calculador.CalcularResultado(primerNumero, operador, segundoNumero);
+                Console.WriteLine($"El resultado es: {resultado}");
             }
         }
     }
